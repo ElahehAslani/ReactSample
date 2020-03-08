3cd1a140c3634696dd00e1490b998fc708eeba3c
eli aslani <elahehaslan01@gmail.com>
2020-03-09 01:12:00 +0330

    first commit for test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Eli Aslani
         </a>
       </header>
     </div>
