39765909fa5485d4a992675fe54d83dc735155dd
eli aslani <elahehaslan01@gmail.com>
2020-03-09 18:50:17 +0330

    input add

--- src/App.js
+++ src/App.js
@@ -2,15 +2,40 @@ import React from 'react';
 import Users from './Users';
 
 class App extends React.Component {
-    render() {
-      return (
-        <div>
-          <Users />
-          <Users />
-        </div>
-        
-      )
+
+  constructor(props) {
+    super(props);
+    this.updateInputValue = this.updateInputValue.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      value : "",
+      inputValue : ""
     }
+  }
+
+  updateInputValue(evnt){
+    this.setState({
+      inputValue : event.target.value
+    })
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    return {
+      value : parseInt(this.state.inputValue)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <input value={this.state.inputValue} onChange={this.updateInputValue}></input>
+          <button onClick={this.handleSubmit}>Submit</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
