76e5b54464f7174de3737811042cf879fbb16345
eli aslani <elahehaslan01@gmail.com>
2020-03-12 15:45:20 +0330

    link

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import React from 'react';
+import './App.css'
 import Users from './Users';
-import { BrowserRooter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends React.Component {
 
   render() {
 
+    const Home = () => {
+      return (
+        <div>Home</div>
+      )
+    }
+
+    const About = () => {
+      return (
+        <div>About</div>
+      )
+    }
+
+    const Contact = () => {
+      return (
+        <div>Contact</div>
+      )
+    }
+
+    const Menu =() => {
+      return (
+        <div>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
+          <Link to="/about">
+            <button>About</button>
+          </Link>
+          <Link to="/contact">
+            <button>Contact</button>
+          </Link>
+        </div>
+      )
+    }
+
     return (
       <Router>
         <div>
           <h1>REACT Router</h1>
+          <Menu />
           <div>
             <Route exact path="/" component={Home}></Route>
             <Route exact path="/about" component={About}></Route>
